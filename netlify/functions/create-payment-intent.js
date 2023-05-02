3af7c861395f9b4b202f7b8d2d0eb1bc7c9980eb
Jeison <jeysonm021@gmail.com>
2023-05-02 15:56:01 -0500

    fixing errors with stripe

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function handler(event, context) {
-  ////console.log(event);
-  ////console.log(context);
   try {
     const { amount } = JSON.parse(event.body);
 
@@ -16,11 +14,14 @@ export async function handler(event, context) {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
       body: JSON.stringify({ paymentIntent }),
     };
   } catch (error) {
-    //console.log({ error });
-
     return {
       statusCode: 400,
       body: JSON.stringify({ error }),
--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -3,5 +3,7 @@ import { loadStripe } from "@stripe/stripe-js";
 ////console.log(import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY);
 //const key = `${apiKey}:${import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY}`;
 export const stripePromise = loadStripe(
-  import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY
+  `${import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY}`
 );
+
+// `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`;
