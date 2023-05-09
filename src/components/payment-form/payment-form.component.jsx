fcdbf200c50e77990eec2bc4aea6d8a4a239f25c
Jeison <jeysonm021@gmail.com>
2023-05-09 16:18:14 -0500

    login fix

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,7 @@
 import { useSelector } from "react-redux";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { clearCart } from "../../redux/cart.reducer";
-import { useDispatch } from "react-redux";
 import PaypalBtn from "../paypal/PaypalBtn";
 
 export default function PaymentForm() {
-  const currentUser = useSelector((state) => state.user.currentUser);
   const { cartItems } = useSelector((state) => state.cart);
   const cartTotal = useSelector((state) => state.cart.cartTotal);
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,8 +3,6 @@ import "./shop.style.scss";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import CategoryComponent from "../category/category.component";
 
-import { ProductsContext } from "../../contexts/product.context";
-
 export default function ShopComponent() {
   //TODO use cashing with swr or react-query to cache the data
   return (
